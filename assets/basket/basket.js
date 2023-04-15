51df2e0db775a2d2a8a24ac917bdac62bc368744
FoxyRaspberry <hogwartsgsrh97@gmail.comm>
2023-04-15 23:33:15 +0300

    feat: allow deletion of products

--- assets/basket/basket.js
+++ assets/basket/basket.js
@@ -1,14 +1,13 @@
 class BasketComponent {
   constructor(basketOrderElement) {
     this.basketOrderElement = basketOrderElement;
-    this.list = [];
+    this.productsMap = new Map();
     this.totalPrice = 0;
     this.initializationView();
   }
 
   // Public.
   addProduct(book) {
-    this.list.push(book);
     this.appendCard(book);
     this.totalPrice += book.price;
     this.totalPriceElement.innerText = 'Total price: $ ' + this.totalPrice;
@@ -17,15 +16,25 @@ class BasketComponent {
 
   // Private.
   appendCard(book) {
-    const card = createBasketBookCard(book);
-    this.listElement.appendChild(card);
+    const cardElement = createBasketBookCard(book);
+    this.productsMap.set(cardElement, book);
+    this.listElement.appendChild(cardElement);
+  }
+
+  deleteProduct(cardElement) {
+    const book = this.productsMap.get(cardElement);
+    this.productsMap.delete(cardElement);
+    cardElement.remove();
+    this.totalPrice -= book.price;
+    this.totalPriceElement.innerText = 'Total price: $ ' + this.totalPrice;
+    this.dispatchEvent();
   }
 
   // Создать пользовательское событие с данными о текущем количестве товаров.
   dispatchEvent() {
     const productsCountChangedEvent = new CustomEvent('productsCountChanged', {
       detail: {
-        count: this.list.length,
+        count: this.productsMap.size,
       },
     });
     this.basketOrderElement.dispatchEvent(productsCountChangedEvent);
@@ -38,6 +47,14 @@ class BasketComponent {
 
     this.listElement = document.createElement('div');
     this.listElement.classList.add('book-shop-basket__list');
+    // Обработать все клики внутри элемента для вывода списка карточек.
+    // Клики по кнопкам интерпретировать как команду для удаления родителькой карточки и соответствующего товара.
+    this.listElement.addEventListener('click', (pointerEvent) => {
+      if (!pointerEvent.target.classList.contains('book-card-basket__button-close')) {
+        return;
+      }
+      this.deleteProduct(pointerEvent.target.parentElement);
+    });
     containerElement.appendChild(this.listElement);
 
     this.totalPriceElement = document.createElement('div');
