846b679461661d9adf9298492288efe3a471def3
FoxyRaspberry <hogwartsgsrh97@gmail.comm>
2023-04-17 23:10:47 +0300

    feat: set the constraints

---
+++ delivery-form/form.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', () => {
+  function getMinDeliveryDate() {
+    const tomorrow = new Date();
+    tomorrow.setDate(tomorrow.getDate() + 1);
+    const tomorrowDateString = (tomorrow.getDate() + '').padStart(2, '0');
+    const tomorrowMonthString = (tomorrow.getMonth() + 1 + '').padStart(2, '0');
+    return `${tomorrow.getFullYear()}-${tomorrowMonthString}-${tomorrowDateString}`;
+  }
+
+  const deliveryDateElement = document.getElementsByClassName('delivery__date')[0];
+  deliveryDateElement.setAttribute('min', getMinDeliveryDate());
+
+  const formElement = document.forms[0];
+  formElement.addEventListener('focusout', (event) => {
+    if (event.target.classList.contains('delivery__input')) {
+      const inputType = event.target.getAttribute('type');
+      if (inputType === 'text') {
+        event.target.classList.add('delivery__input-text--touched');
+      } else if (inputType === 'date') {
+        event.target.classList.add('delivery__input-date--touched');
+      }
+    }
+  });
+});
